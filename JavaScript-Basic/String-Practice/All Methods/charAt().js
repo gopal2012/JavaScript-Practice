0f3dd18318f9ad2b09f50e12bf2cc6fcd132cc91
Gopal <156323763+gopal2012@users.noreply.github.com>
2024-06-17 07:26:45 +0000

    practice on charAt() method

---
+++ JavaScript-Basic/String-Practice/All Methods/charAt().js
@@ -0,0 +1,40 @@
+// let text = "Hello, World!";
+// let letter = text.charAt(0); // returns 'H'
+// console.log(letter);
+
+//Exercise
+
+// 1. What will be the output of the following code?
+
+// let greeting = "Hello, World!";
+// console.log(greeting.charAt(greeting.length - 1)); // return !
+
+
+// 2. Consider the string `let fruits = "Apple, Banana, Cherry";`. Write a line of code that uses `charAt` to find the character at the index equal to half the length of the string.
+
+// let fruits = "Apple, Banana, Cherry";
+// console.log(fruits.charAt(fruits.length/2));
+
+
+// 3. Given the string `let phrase = "The quick brown fox jumps over the lazy dog.";`, use `charAt` to extract and console log the character 'q'.
+
+// let phrase = "The quick brown fox jumps over the lazy dog.";
+// console.log(phrase.charAt(4));
+
+
+// 4. How would you use `charAt` to check if the third character in a string is a vowel?
+
+// let exmp = "i am a boy";
+// let vowel = ["a", "e" , "i" , "o" , "u"];
+//   let y;
+//     for(let i = 0 ; i < 5 ; i++) {
+//         if (exmp.charAt(3) == vowel) {
+//             y = 1;
+//         }
+//     }
+//     if (y = 1) {
+//         console.log("This character is vowel");
+//     }
+//     else {
+//         console.log("This character is not vowel");
+//     }
