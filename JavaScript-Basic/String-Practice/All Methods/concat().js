2a76d010c75ee7b8311ebb26aacec0a2918fccdb
Gopal <156323763+gopal2012@users.noreply.github.com>
2024-06-17 07:59:19 +0000

    practice

---
+++ JavaScript-Basic/String-Practice/All Methods/concat().js
@@ -0,0 +1,48 @@
+// Define two strings
+// const string1 = "Hello, ";
+// const string2 = "World!";
+
+// Use the concat() method to merge them
+// const mergedString = string1.concat(string2);
+
+// Output the result
+// console.log(mergedString); // "Hello, World!"
+
+// ------------------------------------------------------------------------
+// EXERCISE 
+//-------------------------------------------------------------------------
+
+//1. Concatenate the following strings:
+// const str1 = "JavaScript ";
+// const str2 = "is ";
+// const str3 = "fun!";
+// Use the concat() method to combine these strings into one.
+// What is the resulting string?
+// const result = str1.concat(str2.concat(str3)); // or const result = str1.concat(str2, str3);
+// console.log(result);
+
+//2. Concatenate the following strings:
+// const greeting = "Hello";
+// const separator = ", ";
+// const name = "Alice";
+// const punctuation = "!";
+// Use the concat() method to create a single string that says "Hello, Alice!".
+// console.log(greeting.concat(separator, name , punctuation));
+
+
+//3. Concatenate the following strings:
+const firstName = "John";
+const lastName = "Doe";
+const space = " ";
+// Use the concat() method to create a single string that combines the first and last name with a space in between.
+console.log(firstName.concat(space, lastName));
+
+
+
+
+
+
+
+
+
+
