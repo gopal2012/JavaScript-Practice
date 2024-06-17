311b5cf98bd87fe468b1de8f0f3623690c456632
Gopal <156323763+gopal2012@users.noreply.github.com>
2024-06-17 08:52:20 +0000

    Practice on concat method

--- JavaScript-Basic/String-Practice/All Methods/concat().js
+++ JavaScript-Basic/String-Practice/All Methods/concat().js
@@ -31,17 +31,69 @@
 
 
 //3. Concatenate the following strings:
-const firstName = "John";
-const lastName = "Doe";
-const space = " ";
+// const firstName = "John";
+// const lastName = "Doe";
+// const space = " ";
 // Use the concat() method to create a single string that combines the first and last name with a space in between.
-console.log(firstName.concat(space, lastName));
+// console.log(firstName.concat(space, lastName));
+
+
+//4. Concatenate the following strings and numbers:
+// const str = "The year is ";
+// const year = 2024;
+// Use the concat() method to create a single string that says "The year is 2024".
+// console.log(str.concat(year));
+
+//5. Concatenate the following strings using chaining:
+// const str1 = "Learning ";
+// const str2 = "JavaScript ";
+// const str3 = "is ";
+// const str4 = "fun!";
+// Use the concat() method to combine these strings into one, using chaining.
+// const result = str1.concat(str2).concat(str3).concat(str4);
+// console.log(result);
+
+//6. Concatenate the following strings with spaces in between:
+// const word1 = "Learning";
+// const word2 = "to";
+// const word3 = "code";
+// Use the concat() method to create a single string that says "Learning to code".
+
+
+//7. Concatenate the following strings, including an empty string:
+// const str1 = "Concatenating";
+// const emptyStr = "";
+// const str2 = "with";
+// const str3 = "an";
+// const str4 = "empty";
+// const str5 = "string.";
+// Use the concat() method to create a single string.
+// console.log(str1.concat(emptyStr," ",str2," ",str3," ",str4," ",str5));
+
+
+//8. Concatenate the following strings with special characters:
+// const part1 = "Special";
+// const part2 = " characters: ";
+// const part3 = "!, @, #, $";
+// Use the concat() method to create a single string.
+// console.log(part1.concat(part2,part3));
 
 
 
+//9. Concatenate the following parts of a sentence:
+// const subject = "The quick brown fox";
+// const verb = " jumps over";
+// const object = " the lazy dog.";
+// Use the concat() method to create the full sentence.
+// console.log(subject.concat(verb,object));
 
 
 
+//10. Concatenate the following strings repeatedly:
+// const base = "Echo ";
+// const exclamation = "echo!";
+// Use the concat() method to create a single string that says "Echo echo! Echo echo! Echo echo!".
+// console.log(base.concat(exclamation));
 
 
 
