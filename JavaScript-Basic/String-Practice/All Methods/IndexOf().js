2f9fd006acb543e5aa25fb10aada5f6a74810157
Gopal Gorain <156323763+gopal2012@users.noreply.github.com>
2024-06-18 13:40:51 +0530

    upload

---
+++ JavaScript-Basic/String-Practice/All Methods/IndexOf().js
@@ -0,0 +1,35 @@
+// Definition: The indexOf() method returns the index within the calling String object of the first occurrence of the specified value, starting the search at fromIndex. Returns -1 if the value is not found.
+
+// syntex - str.indexOf(searchValue, fromIndex)
+
+//Example - 
+// const str = "Hello, world!";
+// const index = str.indexOf("world");
+// console.log(index); // 7
+
+//----------------------------------
+//Five Important Coding Questions:
+//----------------------------------
+
+//1.Find the first occurrence of the word "JavaScript" in the string "Learning JavaScript is fun!"
+// const str = "Learning JavaScript is fun!";
+// const findWord = "JavaScript";
+// const result = str.indexOf("JavaScript");
+// console.log(result); //9
+
+//2.Get the substring "fox" from the string "The quick brown fox"
+// const str = "The quick brown fox";
+// const findWord = "fox";
+// console.log(str.indexOf(findWord)); //16
+
+// Find the first occurrence of the word "JavaScript" in the string "Learning JavaScript is fun!"
+
+
+// Determine if the string "OpenAI" is present in the string "We are using OpenAI's GPT-4."
+
+// Find the position of the first space in the string "No spaces here"
+
+// Check if the substring "apple" is in the string "I like apples and bananas" starting from index 10
+
+// Find the index of the letter 'e' in the string "Hello, world!" after index 5
+
